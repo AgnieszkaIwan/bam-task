eb2031c06561c91e7b197d17bdf203ebc288353a
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2025-07-06 19:24:59 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/bam-task/", // ważne! dopasuj do nazwy repozytorium na GitHub Pages
-  root: ".", // katalog główny projektu
-  publicDir: "public", // folder na statyczne pliki (obrazy, video itd.)
-
+  base: "/bam-task/",
+  root: ".",
+  publicDir: "public",
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"), // alias do folderu src
+      "@": resolve(__dirname, "src"),
     },
   },
 
@@ -18,7 +17,6 @@ export default defineConfig({
     rollupOptions: {
       input: resolve(__dirname, "index.html"),
       output: {
-        // Wszystkie pliki JS i CSS w folderze assets z hashami dla cache bustingu
         entryFileNames: "assets/[name]-[hash].js",
         chunkFileNames: "assets/[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash][extname]",
